3538d6742ec23564c174f53fadbb7f9ffc135db5
HongyuHe <2290642829@qq.com>
2019-03-17 07:37:42 +0100

    Template

---
+++ cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_6a518.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_6a518.dir/testCCompiler.c.obj"
+  "cmTC_6a518.pdb"
+  "cmTC_6a518.exe"
+  "cmTC_6a518.exe.manifest"
+  "libcmTC_6a518.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_6a518.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
